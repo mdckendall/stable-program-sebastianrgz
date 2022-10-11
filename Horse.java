d8883088da4adea9683d1a76c0ac348bd9bf49eb
sebastianrgz <113557390+sebastianrgz@users.noreply.github.com>
2022-10-11 02:02:45 +0000

    HW

--- Horse.java
+++ Horse.java
@@ -1,1 +1,17 @@
 //Create the Horse class here.
+public class Horse{
+
+String name;
+double weight;
+boolean tame;
+
+public Horse(String name, double weight, boolean tame){
+  this.name = name;
+  this.weight = weight;
+  this.tame = tame;
+ } 
+@Override
+  public String toString(){
+    return "Name: " + name + " Weight: " + weight + " Tame: " + tame;
+  }
+}
